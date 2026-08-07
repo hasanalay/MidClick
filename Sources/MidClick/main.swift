cac3583d00db479fb3370952087901173ff1636d
Hasan ALAY <hasan.alay@outlook.com>
2026-08-08 00:35:15 +0300

    fix: isolate AppKit bootstrap from top-level code

--- Sources/MidClick/main.swift
+++ Sources/MidClick/main.swift
@@ -1,6 +1,10 @@
 import AppKit
 
-let application = NSApplication.shared
-let appDelegate = AppDelegate()
-application.delegate = appDelegate
-application.run()
+func main() {
+    let appDelegate = AppDelegate()
+    let application = NSApplication.shared
+    application.delegate = appDelegate
+    application.run()
+}
+
+main()
